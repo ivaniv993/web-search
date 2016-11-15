d1751222a7c0731481b4b8aafbf7beb8ccab69d4
ivaniv993 <ivanyv.ivan@yandex.ua>
2016-11-15 05:40:07 +0200

    security in memory

--- src/main/java/com/edu/controllers/MainController.java
+++ src/main/java/com/edu/controllers/MainController.java
@@ -1,22 +1,22 @@
 package com.edu.controllers;
 
 import com.edu.domain.Job;
-import com.edu.domain.User;
 import com.edu.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContext;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 
 /**
@@ -36,31 +36,20 @@ public class MainController {
     private UserService userService;
 
     @RequestMapping(value = "/hello", method= RequestMethod.GET)
-    public List<User> upload( ) throws IOException {
+    public Set<Job> upload( ) throws IOException {
 
         log.info("bla bla");
-//        SecurityContext ctx = SecurityContextHolder.getContext();
-//        Authentication authentication = ctx.getAuthentication();
-//
-//        User custom = authentication == null ? null : (User) authentication.getPrincipal();
-//
-//        log.info("Authenticated user:  {} ", custom.getUsername());
-//        log.info("Contact :  {} ", contactService.findAll());
-        List<User> contacts = userService.findAll();
+        SecurityContext ctx = SecurityContextHolder.getContext();
+        Authentication authentication = ctx.getAuthentication();
+
+        User custom = authentication == null ? null : (User) authentication.getPrincipal();
+
+        log.info("Authenticated user:  {} ", custom.getUsername());
 
 //        contacts.get(0).getJobs();
-        return contacts;
-    }
 
-    @RequestMapping(value = "/user", method= RequestMethod.GET)
-    public void saveUser( ) throws IOException {
-
-        User contact = new User();
-        contact.setFirstName("Ivan");
-        contact.setLastName("Ivaniv");
-        contact.setBirthDate(new Date());
-
-        userService.save(contact);
+        // TODO convert entity to DTO
+        return userService.findByFirstName(custom.getUsername()).getJobs();
     }
 
 }
--- src/main/java/com/edu/security/MvcWebApplicationInitializer.java
+++ src/main/java/com/edu/security/MvcWebApplicationInitializer.java
@@ -1,26 +1,26 @@
 package com.edu.security;
 
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
  * Created by iivaniv on 19.07.2016.
  */
 
-public class MvcWebApplicationInitializer /*extends AbstractAnnotationConfigDispatcherServletInitializer */{
-//    @Override
-//    protected Class<?>[] getRootConfigClasses() {
-//
-//        return new Class[] { SecurityConfig.class } ;
-//    }
-//
-//    @Override
-//    protected Class<?>[] getServletConfigClasses() {
-//        return new Class<?>[0];
-//    }
-//
-//    @Override
-//    protected String[] getServletMappings() {
-//        return new String[0];
-//    }
+public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+
+        return new Class[] { SecurityConfig.class } ;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
